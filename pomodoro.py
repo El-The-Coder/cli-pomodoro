f87c5c1b49dbfae8b40ca70ecfd3b44a5a84478f
/Lmrzx <121160077+El-The-Coder@users.noreply.github.com>
2023-06-30 22:26:15 +0700

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -14,23 +14,29 @@ def play_sound(sound_path):
     pygame.mixer.music.play()
 
 def play_start_sound():
-    play_sound('/path/to/start_sound.wav') #isi pake suara buat nandain pomodoro dimulai
+    play_sound('/path/to/start_sound.wav')
 
 def play_short_break_sound():
-    play_sound('/path/to/short_break_sound.wav') #isi pake suara buat nandain istirahat pendek dimulai
+    play_sound('/path/to/short_break_sound.wav')
 
 def play_long_break_sound():
-    play_sound('/path/to/long_break_sound.wav') #isi pake suara buat nandain istirahat panjang dimulai
+    play_sound('/path/to/long_break_sound.wav')
 
 def play_finish_sound():
-    play_sound('/path/to/finish_sound.wav') #isi pake suara buat nandain pomodoro selesai
+    play_sound('/path/to/finish_sound.wav')
 
 def show_notification(title, message):
     notify2.init("Pomodoro")
     notification = notify2.Notification(title, message)
     notification.show()
 
-def pomodoro_timer(duration, remaining_pomodoros):
+def save_history(activity, pomodoro_count):
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    history_entry = f"{timestamp}: {pomodoro_count} pomodoro(s) for activity: {activity}\n"
+    with open("pomodoro_history.txt", "a") as file:
+        file.write(history_entry)
+
+def pomodoro_timer(duration, remaining_pomodoros, activity):
     start_time = time.time()
     end_time = start_time + duration
     print("Pomodoro dimulai!")
@@ -49,6 +55,7 @@ def pomodoro_timer(duration, remaining_pomodoros):
         else:
             play_finish_sound()
             show_notification("Pomodoro Selesai!", "Semua pomodoro selesai!")
+        save_history(activity, remaining_pomodoros)
     except KeyboardInterrupt:
         print("\n\nPomodoro dihentikan!")
 
@@ -89,13 +96,15 @@ def long_break_timer(duration):
 def start_pomodoro():
     activity = input("Masukkan aktivitas: ")
     pomodoro_count = int(input("Berapa kali pomodoro akan dilakukan? "))
+    play_start_sound()
+    save_history(activity, pomodoro_count)
     pomodoro_duration = 25 * 60
     short_break_duration = 5 * 60
     long_break_duration = 20 * 60
     remaining_pomodoros = pomodoro_count
 
     while remaining_pomodoros > 0:
-        pomodoro_thread = threading.Thread(target=pomodoro_timer, args=(pomodoro_duration, remaining_pomodoros))
+        pomodoro_thread = threading.Thread(target=pomodoro_timer, args=(pomodoro_duration, remaining_pomodoros, activity))
         pomodoro_thread.start()
         pomodoro_thread.join()
 
@@ -119,5 +128,4 @@ def signal_handler(signal, frame):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
-    play_start_sound()
     start_pomodoro()
